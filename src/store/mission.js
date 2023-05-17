6b7664b5f45fcc156ced3989c824563c4fb77819
Alvaro Poma <arpoma167@gmail.com>
2023-05-17 16:18:21 +0200

    obtencion de idle_vel de mission aun no se pasa al uav

--- src/store/mission.js
+++ src/store/mission.js
@@ -29,9 +29,21 @@ const { reducer, actions } = createSlice({
           state.route[n_uav]['wp'] = {} ;
           state.route[n_uav]['attributes'] = {} ;
           state.home = action.payload.mission["uav_"+n_uav]['wp_0'];
+          //globa Attributes
+          if (action.payload.mission.hasOwnProperty('mode_landing')){
+            state.route[n_uav]['attributes']['mode_landing'] = action.payload.mission["mode_landing"];  
+          }
+          if (action.payload.mission.hasOwnProperty('mode_yaw')){
+            state.route[n_uav]['attributes']['mode_yaw'] = action.payload.mission["mode_yaw"];  
+          }
+          if (action.payload.mission.hasOwnProperty('idle_vel')){
+            state.route[n_uav]['attributes']['idle_vel'] = action.payload.mission["idle_vel"];  
+          }
+          //waipoints
           for(let wp_n = 0; wp_n < action.payload.mission["uav_"+n_uav]['wp_n']; wp_n++){
             state.route[n_uav]['wp'][wp_n] = action.payload.mission["uav_"+n_uav]['wp_'+wp_n];
           }
+          //
           if (action.payload.mission["uav_"+n_uav].hasOwnProperty('attributes')){
             console.log("have atribute")
             if (action.payload.mission["uav_"+n_uav]['attributes'].hasOwnProperty('mode_landing')){
@@ -41,14 +53,20 @@ const { reducer, actions } = createSlice({
             if (action.payload.mission["uav_"+n_uav]['attributes'].hasOwnProperty('mode_yaw')){
               state.route[n_uav]['attributes']['mode_yaw'] = action.payload.mission["uav_"+n_uav]['attributes']["mode_yaw"];  
             }
+            if (action.payload.mission["uav_"+n_uav]['attributes'].hasOwnProperty('idle_vel')){
+              state.route[n_uav]['attributes']['idle_vel'] = action.payload.mission["uav_"+n_uav]['attributes']["idle_vel"];  
+            }
           }else{
-            if (action.payload.mission.hasOwnProperty('mode_landing')){
-              state.route[n_uav]['attributes']['mode_landing'] = action.payload.mission["mode_landing"];  
+            if (action.payload.mission["uav_"+n_uav].hasOwnProperty('mode_landing')){
+              console.log("have modelanding"+n_uav)
+              state.route[n_uav]['attributes']['mode_landing'] = action.payload.mission["uav_"+n_uav]["mode_landing"];  
             }
-            if (action.payload.mission.hasOwnProperty('mode_yaw')){
-              state.route[n_uav]['attributes']['mode_yaw'] = action.payload.mission["mode_yaw"];  
+            if (action.payload.mission["uav_"+n_uav].hasOwnProperty('mode_yaw')){
+              state.route[n_uav]['attributes']['mode_yaw'] = action.payload.mission["uav_"+n_uav]["mode_yaw"];  
             }
-            
+            if (action.payload.mission["uav_"+n_uav].hasOwnProperty('idle_vel')){
+              state.route[n_uav]['attributes']['idle_vel'] = action.payload.mission["uav_"+n_uav]["idle_vel"];  
+            }
           }
         }
 
