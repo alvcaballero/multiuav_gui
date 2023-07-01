9b2a244cb748e0d080213eee7843f85f62f50f1f
Alvaro Poma <arpoma167@gmail.com>
2023-07-01 10:48:46 +0200

    cambios  add uav

--- src/components/Adduav.js
+++ src/components/Adduav.js
@@ -2,7 +2,7 @@ import React,{ useContext,useState } from 'react'
 import { RosContext } from './RosControl'
 import useClasses from './useClasses'
 import CloseIcon from '@mui/icons-material/Close';
-
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {
   Card,
   IconButton,
@@ -16,9 +16,9 @@ import {
   AccordionSummary,
   AccordionDetails,
   Typography,
+  FormGroup,
   FormControlLabel,
   Checkbox,
-  TextField,
 } from '@mui/material';
 
 const styles = theme => ({
@@ -48,21 +48,21 @@ const styles = theme => ({
     position: 'fixed',
     zIndex: 6,
     left: '50%',
-    top: '35%',
+    top: '30%',
     transform: 'translateX(-50%)',
   },
   button: {
     width: '80%',
     paddingBottom:'10pt',
+    paddingTop:'10pt'
   },
   formControl: {
     margin: theme.spacing(1),
-    width: '80%',
-    minWidth: 120,
+    gap: theme.spacing(2),
     paddingBottom:'20pt',
   },
   inputtext:{
-    paddingBottom:'10pt',
+    paddingBottom:'20px',
   },
     details: {
     display: 'flex',
@@ -76,8 +76,11 @@ const styles = theme => ({
 export const Adduav = ({SetAddUAVOpen}) => {
     const classes = useClasses(styles);
     const rosContex = useContext(RosContext);
-    const [uavtype, setuavtype] = useState('dji');
+    const [uavtype, setuavtype] = useState("dji");
     const [uavid, setuavid] = useState('');
+    const [item, setItem] = useState({});
+
+    const validate = () => item && item.name && item.uniqueId;
 
 	function showAddUav(){
 		if(document.getElementById("AddUav").style.width === "250px"){
@@ -105,56 +108,74 @@ export const Adduav = ({SetAddUAVOpen}) => {
   return (
     <div className={classes.root}>
     <Card elevation={3} className={classes.card}>
-          <div style={{display:'flex',right:"5px",height:"35px",position:"absolute"}}>
-            <IconButton size="small" onClick={closeAddUav} onTouchStart={closeAddUav}>
-              <CloseIcon fontSize="small" className={classes.mediaButton} />
-            </IconButton>
-          </div>
+      <div style={{display:'flex',right:"5px",height:"35px",position:"absolute"}}>
+        <IconButton size="small" onClick={closeAddUav} onTouchStart={closeAddUav}>
+          <CloseIcon fontSize="small" className={classes.mediaButton} />
+        </IconButton>
+      </div>
+    
+      <b><div style={{display: 'block',width:"calc( 100% - 60pt )",paddingLeft:"15pt",paddingTop:"10pt",paddingBottom:"20pt",textAlign:"left"}}>Add device  </div></b>
         
-          <b><div style={{display: 'block',width:"calc( 100% - 60pt )",paddingLeft:"15pt",paddingTop:"10pt",paddingBottom:"20pt",textAlign:"left"}}>Add device  </div></b>
-            <TextField
+      <FormGroup style={{margin:"20px"}}>
+          <TextField
             required
             label="UAV ID"
             name="uavid"
             value={uavid}
-            onChange={(e) => setuavid(e.target.value)} className={classes.inputtext}
+            onChange={(e) => setuavid(e.target.value)} 
+            className={classes.inputtext}
           />
-      <FormControl variant="outlined" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-outlined-label">UAV type</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={uavtype}
-          onChange={(e) => setuavtype(e.target.value)}
-          label="uavtipe"
-        >
-          <MenuItem value="dji">DJI</MenuItem>
-          <MenuItem value="px4">PX4</MenuItem>
-          <MenuItem value="fuvex">Fuvex</MenuItem>
-          <MenuItem value="catec">Catec</MenuItem>
-        </Select>
-      </FormControl>
-      <Accordion defaultExpanded>
+
+        <FormControl style={{paddingBottom:"20px"}} variant="outlined" >
+          <InputLabel id="demo-simple-select-outlined-label">UAV type</InputLabel>
+          <Select
+            labelId="demo-simple-select-outlined-label"
+            id="demo-simple-select-outlined"
+            value={uavtype}
+            onChange={(e) => setuavtype(e.target.value)}
+            label="uavtipe"
+          >
+            <MenuItem value="dji">DJI</MenuItem>
+            <MenuItem value="px4">PX4</MenuItem>
+            <MenuItem value="fuvex">Fuvex</MenuItem>
+            <MenuItem value="catec">Catec</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField 
+          value={'10.42.0.42'}
+          onChange={(event) => setItem({ ...item, ip: event.target.value })}
+          label='ip'
+        />
+      </FormGroup>
+      {item &&  <Accordion >
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography variant="subtitle1">icon
+              <Typography variant="subtitle1">Camera
               </Typography>
             </AccordionSummary>
             <AccordionDetails className={classes.details}>
+            <FormControl variant="outlined" >
+              <InputLabel id="demo-simple-select-outlined-label">CameraSource</InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value="Websocket"
+                onChange={(e) => setuavtype(e.target.value)}
+                label="uavtipe"
+              >
+                <MenuItem value="Websocket">Websocket</MenuItem>
+                <MenuItem value="WebRTC">WebRTC</MenuItem>
+              </Select>
+            </FormControl>
               <TextField
-                value={item.name || ''}
-                onChange={(event) => setItem({ ...item, name: event.target.value })}
-                label='icon'
-              />
-              <TextField
-                value={item.uniqueId || ''}
+                value={'videocompress'}
                 onChange={(event) => setItem({ ...item, uniqueId: event.target.value })}
-                label='icon'
-                helperText='icon'
+                label='Camera route'
+                helperText='videocompress or video0  '
               />
             </AccordionDetails>
-          </Accordion>
+          </Accordion>}
   
-      <div style={{paddingBottom: '20pt'}}>
+      <div style={{paddingBottom: '20pt',paddingTop: '20pt'}}>
         <Button className={classes.button} variant="contained" onClick={AddnewUAV}>ADD</Button>
         </div>                
 
