7154f47619423fe42744b6315bc102f98889746b
arpa <arpoma167@gmail.com>
2023-08-10 16:29:39 +0200

    build

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 //import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
-import preloadImages from './Mapview/preloadImages';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
-import Navigation from './Navigation';
+import { Provider } from "react-redux";
+import store from "./store";
+import preloadImages from "./Mapview/preloadImages";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
+import Navigation from "./Navigation";
 
-import AppThemeProvider from './AppThemeProvider';
+import AppThemeProvider from "./AppThemeProvider";
 
 preloadImages();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-     <Provider store={store}>
-        <StyledEngineProvider injectFirst>
-          <AppThemeProvider>
-            <CssBaseline />
-            <BrowserRouter>
-                <Navigation />
-            </BrowserRouter>
-          </AppThemeProvider>
-        </StyledEngineProvider>
-     </Provider>
+  <Provider store={store}>
+    <StyledEngineProvider injectFirst>
+      <AppThemeProvider>
+        <CssBaseline />
+        <BrowserRouter>
+          <Navigation />
+        </BrowserRouter>
+      </AppThemeProvider>
+    </StyledEngineProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
