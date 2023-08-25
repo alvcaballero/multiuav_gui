bde04de00d8135611b030435c0e94663d61270a8
arpa <arpoma167@gmail.com>
2023-08-25 16:58:08 +0200

    esto no deberia ser un commit a la rama master, no descargar, semi cambios en  mission page

--- src/components/MissionElevation.jsx
+++ src/components/MissionElevation.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import * as turf from "@turf/turf"; //import { length } from "@turf/length";
 import {
   CartesianGrid,
   Line,
@@ -12,7 +13,8 @@ import {
 } from "recharts";
 
 import { makeStyles } from "@mui/styles";
-
+//https://www.opentopodata.org/
+//https://open-elevation.com/
 //https://codepen.io/tobinbradley/pen/jOeRbwr
 // https://github.com/traccar/traccar-web/blob/667d0f68daaa3916fac6653ef9994f6f2d05e177/modern/src/reports/ChartReportPage.jsx#L8
 const useStyles = makeStyles((theme) => ({
@@ -22,40 +24,114 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
   },
 }));
+
 const MissionElevation = () => {
   const classes = useStyles();
   const [items, setItems] = useState([]);
   const Mission_route = useSelector((state) => state.mission);
 
-
-
-
-   async function setnewaction (){
-    console.log("remove action" + index_route + "-" + index_wp + "-" + action);
+  async function setnewaction() {
     let auxroute = JSON.parse(JSON.stringify(Mission_route.route));
     let listwaypoint = "";
+    let listwp = [];
     if (auxroute.length > 0) {
-      auxroute.array.forEach((route) => {
-        route.wp.forEach((wp) => {
-            listwaypoint = listwaypoint + wp.pos[1], wp.pos[0]];
+      auxroute.forEach((route, index_rt, array_rt) => {
+        let listwp_aux = [];
+        route.wp.forEach((wp, index, array) => {
+          listwaypoint = listwaypoint + wp.pos[0] + "," + wp.pos[1] + "|";
+          listwp_aux.push([wp.pos[0], wp.pos[1]]);
         });
+        listwp.push(listwp_aux);
+        if (array_rt.length - 1 == index_rt) {
+          console.log("delete last value");
+          listwaypoint = listwaypoint.slice(0, -1);
+        }
       });
-      console.log(listwaypoint);
+      console.log("Elevation-----------");
+      console.log(listwp);
+      var linestring = turf.lineString(listwp[0]);
+      let lineLength = turf.length(linestring, { units: "meters" });
+      let divisionLength = lineLength / 20;
+      let newLine = turf.lineChunk(linestring, 10, {
+        units: "meters",
+      }).features;
+      let wp_new = newLine.map((feature, index_ft, list_ft) => {
+        if (list_ft.length - 1 == index_ft) {
+          console.log("delete last value");
+          return (
+            feature.geometry.coordinates[0][0] +
+            "," +
+            feature.geometry.coordinates[0][1] +
+            "|" +
+            feature.geometry.coordinates[1][0] +
+            "," +
+            feature.geometry.coordinates[1][1]
+          );
+        }
+        return (
+          feature.geometry.coordinates[0][0] +
+          "," +
+          feature.geometry.coordinates[0][1] +
+          "|"
+        );
+      });
+
+      console.log(lineLength);
+      console.log(divisionLength);
+      console.log(linestring);
+      console.log(newLine);
+      console.log(wp_new);
     }
 
-    
     let command;
     if (true) {
-      const response = await fetch("/api/mission/actions/dji");
-      if (response.ok) {
-        command = await response.json();
-      } else {
-        throw Error(await response.text());
+      try {
+        const response = await fetch(
+          `/api/map/elevation?locations=${listwaypoint}`
+        );
+        if (response.ok) {
+          command = await response.json();
+        } else {
+          console.log("no ok response");
+          console.log(response);
+          throw new Error(response.status);
+        }
+      } catch (error) {
+        console.log("error");
+        console.log(error);
       }
     }
-    console.log(command);
+    console.log("data elevation");
+    console.log(command.results);
+    //obener array de distancia
+    let mydata = command.results.map((element, index, array) => {
+      console.log(index);
+      let custon_array = [];
+      if (index == 0) {
+        custon_array = [
+          Object.values(command.results[0]["location"]),
+          Object.values(element.location),
+        ];
+      } else {
+        custon_array = [
+          Object.values(command.results[index - 1]["location"]),
+          Object.values(element.location),
+        ];
+      }
 
-  };
+      var linestring = turf.lineString(custon_array);
+
+      let lineLength = turf.length(linestring, { units: "meters" });
+      return { name: lineLength, cost: element.elevation, impression: 2 };
+    });
+    console.log(mydata);
+    setItems(mydata);
+  }
+
+  async function elevation() {
+    console.log("elevation funtion");
+  }
+
   const data = [
     { name: 1, cost: 4.11, impression: 300 },
     { name: 2, cost: 2.39, impression: 120 },
@@ -83,13 +159,17 @@ const MissionElevation = () => {
     setItems(data);
   }, []);
 
+  useEffect(() => {
+    setnewaction();
+  }, [Mission_route]);
+
   return (
     <div style={{ height: "100%", width: "100%" }}>
       {data.length > 0 && (
         <div className={classes.chart}>
           <ResponsiveContainer width="100%" height="100%">
             <LineChart
-              data={data}
+              data={items}
               margin={{
                 top: 10,
                 right: 40,
