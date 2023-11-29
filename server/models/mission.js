244ec2f5eb2e25d67af1eab24a72fe6f9fecc873
Alvaro Poma <arpoma167@gmail.com>
2023-11-29 23:37:20 +0100

    fix problems in ftp

--- server/models/mission.js
+++ server/models/mission.js
@@ -3,9 +3,13 @@ import { DevicesModel } from '../models/devices.js';
 import { readYAML, getDatetime } from '../common/utils.js';
 import ROSLIB from 'roslib';
 import { SFTPClient } from '../common/SFTPClient.js';
+import * as fs from 'fs';
 
 const devices_init = readYAML('../config/devices/devices_init.yaml');
-const sftconections = {};
+const mission = {} // current mission // id , status (init, planing, doing, finish,time inti, time_end))
+const sftconections = {};// manage connection to drone 
+const filestodownload = {}; //manage files that fail download// list of objects,with name, and fileroute, number of try.
+
 
 export class missionModel {
   static getmission() {
@@ -62,7 +66,21 @@ export class missionModel {
     const port = parsedURL.port || 22;
     const { host, username, password } = parsedURL;
     await client.connect({ host, port, username, password });
-    let listfiles = await client.listFiles('.');
+    let listfiles = await client.listFiles('./uav_media/');// que devuelva un  lista de objetos con la fecha de creacion
+    let myfiledownload = 'mission_2023-11-27_13:00'
+    //En lista archivos y directorios, los directorios no se pueden descargar da error, se uede probar errores
+    // crear funcion que solo enliste los archivos
+    // hacer que las funciones devuelvan errores  para poder manejarlos
+    listfiles = await client.listFiles(`./uav_media/${myfiledownload}`);
+    let dir =`/home/arpa/GCS_media/mission_1/${uav_name}`
+    if (!fs.existsSync(dir)){
+      console.log("no exist " + dir)
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    for (let myfile of listfiles) {
+      console.log(myfile);
+      await client.downloadFile(`./uav_media/${myfiledownload}/${myfile}`, `${dir}/${myfile}`);
+    }
     //* Close the connection
     await client.disconnect();
     return listfiles;
@@ -79,7 +97,7 @@ export class missionModel {
     const port = parsedURL.port || 22;
     const { host, username, password } = parsedURL;
     await client.connect({ host, port, username, password });
-    let listfiles = await client.listFiles('.');
+    let listfiles = await client.listFiles('./uav_media/');
     //* Close the connection
     await client.disconnect();
     return listfiles;
