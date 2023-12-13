cf2a0c8ae3633ec7fde36b19aa518c43f41206df
arpa <arpoma167@gmail.com>
2023-12-13 18:32:01 +0100

    fix date time  to devices gtm 0

--- server/models/utils.js
+++ server/models/utils.js
@@ -1,8 +1,8 @@
 export class utilsModel {
   static DateTime() {
     const dateObject = new Date();
-    let stringdate = dateObject.toJSON().slice(0, -1).replace('T',' ');
-    console.log("uav sincronize time"+stringdate)
+    let stringdate = dateObject.toJSON(); //dateObject.toJSON().slice(0, -1).replace('T',' ');
+    console.log('uav sincronize time' + stringdate);
     return { datetime: stringdate };
   }
 }
