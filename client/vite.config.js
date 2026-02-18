ba86ad1c3da06a7ba8fc2015f0d7a00fde04e152
Alvaro Poma <arpoma167@gmail.com>
2026-02-18 22:43:21 +0100

    fic bug of vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(() => ({
   server: {
@@ -13,34 +14,47 @@ export default defineConfig(() => ({
   },
   build: {
     outDir: 'build',
+    target: 'es2022',
+  },
+  esbuild: {
+    target: 'es2022',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022',
+    },
   },
   plugins: [
-    svgr(), 
-    react(), 
+    svgr(),
+    react(),
     VitePWA({
-    includeAssets: ['favicon.ico'],
-    workbox: {
+      includeAssets: ['favicon.ico'],
+      workbox: {
         navigateFallbackDenylist: [/^\/api/],
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,woff,woff2,mp3}'],
       },
-    manifest: {
-      name: 'Drone Control Panel',
-      short_name: 'DCP',
-      description: 'A control panel for managing drone operations',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: 'logo192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: 'logo512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    },
-  })],
+      manifest: {
+        name: 'Drone Control Panel',
+        short_name: 'DCP',
+        description: 'A control panel for managing drone operations',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+    viteStaticCopy({
+      targets: [{ src: 'node_modules/@mapbox/mapbox-gl-rtl-text/dist/mapbox-gl-rtl-text.js', dest: '' }],
+    }),
+  ],
 }));
